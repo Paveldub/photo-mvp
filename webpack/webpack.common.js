a3a1a58963a7bdfac07b7df9b5e0da70f0378790
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-06-11 16:57:12 +0300

    dist dir

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    path: path.resolve(__dirname, '../public'),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js',
   },
   module: {
