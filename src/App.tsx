2976b4194b850a1ed36a4b0cb6f8e8438d4a1a39
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-06-11 17:42:49 +0300

    test title

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,8 @@ export const App = () => {
       <Background>
         <Title>
           React TypeScript Webpack Starter Template - {process.env.NODE_ENV} -
-          {process.env.name} TEST TESTTEST TESTTEST TESTTEST TEST
+          {process.env.name} TEST TESTTEST TESTTEST TESTTEST TESTTEST TESTTEST
+          TESTTEST TESTTEST TESTTEST TESTTEST TESTTEST TESTTEST TEST
         </Title>
         <Button>changed text 111</Button>
         <div>
