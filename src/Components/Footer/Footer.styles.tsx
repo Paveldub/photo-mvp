98fe8751deccc8d0dfa08d699dbb67ac0dc7f829
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-06-14 13:23:38 +0300

    Footer component

--- src/Components/Footer/Footer.styles.tsx
+++ src/Components/Footer/Footer.styles.tsx
@@ -9,9 +9,38 @@ export const Container = styled('div')`
 export const FooterWrapper = styled('div')`
   display: flex;
   justify-content: space-between;
+  margin-top: 50px;
 `
 
 export const FooterHalf = styled('div')`
   width: 50%;
   background: #ccc;
+`
+
+export const SocialIconLink = styled('a')`
+  color: red;
+  font-size: 24px;
+  text-decoration: none;
+  margin-right: 20px;
+
+  &::last-child {
+    margin-right: 0;
+  }
+`
+
+export const SocialIconPic = styled('img')`
+  max-width: 50px;
+  max-height: 50px;
+  src: url(${(props) => props.src});
+`
+
+export const AllRightsReverved = styled('div')`
+  display: flex;
+  align-items: center;
+`
+
+export const DropMeAline = styled('a')`
+  color: red;
+  font-size: 24px;
+  text-decoration: none;
 `
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,12 +1,45 @@
-import { Container, FooterHalf, FooterWrapper } from './Footer.styles'
+import {
+  Container,
+  FooterHalf,
+  FooterWrapper,
+  SocialIconLink,
+  AllRightsReverved,
+  DropMeAline,
+  SocialIconPic,
+} from './Footer.styles'
+
+import instagramPic from '../../images/free-icon-instagram-2111463.svg'
+import telegramPic from '../../images/free-icon-telegram-906377.svg'
 
 export const Footer = () => {
   return (
     <>
       <Container>
         <FooterWrapper>
-          <FooterHalf></FooterHalf>
-          <FooterHalf></FooterHalf>
+          <FooterHalf>
+            <SocialIconLink
+              href="//www.instagram.com/demidovich.fiml/"
+              target="_blank"
+            >
+              <SocialIconPic src={instagramPic} />
+            </SocialIconLink>
+
+            <SocialIconLink href="//t.me/paveldubs" target="_blank">
+              <SocialIconPic src={telegramPic} />
+            </SocialIconLink>
+          </FooterHalf>
+
+          <FooterHalf>
+            <DropMeAline
+              href="mailto: demidovich.fiml@gmail.com"
+              target="_blank"
+            >
+              demidovich.fiml@gmail.com
+            </DropMeAline>
+            <AllRightsReverved>
+              (C) {new Date().getFullYear()} All right reserved
+            </AllRightsReverved>
+          </FooterHalf>
         </FooterWrapper>
       </Container>
     </>
