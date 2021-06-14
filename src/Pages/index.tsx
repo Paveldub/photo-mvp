73652b4a3ce445fb9df77d7d1bced9b4b8dac2a6
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-06-14 17:05:15 +0300

    webpack dev server config

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,4 +1,4 @@
 export { Home } from './Home/Home'
 export { About } from './About/About'
 export { Photography } from './Photography/Photography'
-export { ContactUs } from './Contactus/Contactus'
+export { ContactUs } from './ContactUs/Contactus'
