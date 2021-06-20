cf4e650314159b39954fd595e2317cec74e1857b
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-06-20 17:53:39 +0300

    about page

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -4,9 +4,13 @@ export const About = () => {
   return (
     <Container>
       <h1>About page</h1>
-      <h1> 35 awards </h1>
       <a href="https://35awards.com/author/demidovichfiml/">35 awards</a>
+      <br />
       <a href="https://35awards.com/winners2020/nomination/598/">35 awards</a>
+      <h1>Print Cover</h1>
+      <a href="https://www.thisismob.shop/products/this-is-film-volume-one-issue-1">
+        Cover
+      </a>
     </Container>
   )
 }
