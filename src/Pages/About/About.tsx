39b6967b7925e129d7ab2bc9cc340336d81997ec
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-06-20 10:08:34 +0300

    about page

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -4,6 +4,9 @@ export const About = () => {
   return (
     <Container>
       <h1>About page</h1>
+      <h1> 35 awards </h1>
+      <a href="https://35awards.com/author/demidovichfiml/">35 awards</a>
+      <a href="https://35awards.com/winners2020/nomination/598/">35 awards</a>
     </Container>
   )
 }
