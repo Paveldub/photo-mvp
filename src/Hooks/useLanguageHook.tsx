6833069e1fd0375854c671ac429c96b4b57c014f
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-07-14 02:35:06 +0300

    language works

--- src/Hooks/useLanguageHook.tsx
+++ src/Hooks/useLanguageHook.tsx
@@ -1,6 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { useHistory, useLocation } from 'react-router-dom'
-import { ENGLISH_LANGUAGE } from '../Constants/index'
+import { RUSSIAN_LANGUAGE } from '../Constants/index'
 import { setLanguage } from '../redux/actions/languageActions'
 import { getPathname } from '../utils/getPathName'
 import { setCurrentLanguage } from '../utils/i18n'
@@ -17,8 +17,8 @@ export function useChangeLanguage() {
   const onSetLanguage = (lang: any) => {
     dispatch(setLanguage(lang))
     setCurrentLanguage(lang.value)
-    if (lang.value === ENGLISH_LANGUAGE.value) {
-      history.push(`${ENGLISH_LANGUAGE.path}/${path}`)
+    if (lang.value === RUSSIAN_LANGUAGE.value) {
+      history.push(`${RUSSIAN_LANGUAGE.path}/${path}`)
     } else {
       history.push(`/${path}`)
     }
