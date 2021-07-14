398bf31030ad5d292bbfa912dbf30569bcdd6a90
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-07-14 14:04:48 +0300

    about page

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -7,10 +7,25 @@ export const About = () => {
       <a href="https://35awards.com/author/demidovichfiml/">35 awards</a>
       <br />
       <a href="https://35awards.com/winners2020/nomination/598/">35 awards</a>
-      <h1>Print Cover</h1>
+      <h1>MOB journal</h1>
       <a href="https://www.thisismob.shop/products/this-is-film-volume-one-issue-1">
-        Cover
+        MOB journal
       </a>
+      <h1>Публикации Traaawmag</h1>
+      <a href="https://traaawmag.com/tag/pavel-demidovich/">Traaawmag</a>
+      <h1>C-heads</h1>
+      <a href="https://www.c-heads.com/2020/12/06/warm-vibes-with-celine/">
+        C-heads
+      </a>
+      <h1>Rektmag</h1>
+      <a href="https://www.rektmag.net/home/2020/12/12/belarusian-mermaids">
+        Mermaids
+      </a>
+      <a href="https://www.rektmag.net/home/2020/12/12/selin">Warm Vibes</a>
+      <h1>Kavyar</h1>
+      <a href="https://kavyar.com/pavel-demidovich">Kavyar</a>
+      <h1>Osphilia</h1>
+      <a href="https://www.osphilia.co/sessions/by-paveldemidovich">Osphilia</a>
     </Container>
   )
 }
