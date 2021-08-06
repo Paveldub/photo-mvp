7714df4bf26952afee34f36063395b59a6a9fe23
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-08-06 20:55:19 +0300

    gallery

--- src/Pages/Gallery/Gallery.tsx
+++ src/Pages/Gallery/Gallery.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from 'react'
+import { Slider } from '../../Components'
+import { Loading } from '../../Components/Layout/Loader'
+import db from '../../firebase'
 import { useTranslate } from '../../Hooks/useTranslate'
 import { Container } from './Gallery.styles'
 
@@ -6,11 +10,31 @@ const WORDS = ['galleryText']
 export const Gallery = () => {
   const t = useTranslate(WORDS)
 
+  const [photos, setPhotos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    db.collection('photos').onSnapshot((snapshot) => {
+      setPhotos(
+        snapshot.docs.map((doc) => ({
+          data: doc.data(),
+        }))
+      )
+      setLoading(false)
+    })
+  }, [loading])
+
+  if (loading) {
+    return <Loading />
+  }
+
   return (
     <>
       <Container>
         <h1>{t.galleryText}</h1>
       </Container>
+
+      <Slider photo={photos} />
     </>
   )
 }
