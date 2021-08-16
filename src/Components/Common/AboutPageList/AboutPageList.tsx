7adf7a1eddfa3847bc3f05e54414eacef2363882
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-08-16 17:09:07 +0300

    about page list key

--- src/Components/Common/AboutPageList/AboutPageList.tsx
+++ src/Components/Common/AboutPageList/AboutPageList.tsx
@@ -1,3 +1,5 @@
+import './styles.scss'
+
 export const AboutPageList = (props) => {
   const { aboutData } = props
 
@@ -5,15 +7,21 @@ export const AboutPageList = (props) => {
     <ul>
       {aboutData?.map((item) => {
         return (
-          <>
-            <li key={item.data.id}>
-              <h2>{item.data.magazine_title}</h2>
-              <div className="about-page-urls">
-                <a href={item.data.magazine_url}>{item.data.url_title}</a>
-                <a href={item.data.magazine_url2}>{item.data.url_title2}</a>
-              </div>
-            </li>
-          </>
+          <li key={item.data.id}>
+            <h2>{item.data.magazine_title}</h2>
+            <div className="about-page-urls">
+              <a href={item.data.magazine_url} target="_blank" rel="noreferrer">
+                {item.data.url_title}
+              </a>
+              <a
+                href={item.data.magazine_url2}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {item.data.url_title2}
+              </a>
+            </div>
+          </li>
         )
       })}
     </ul>
