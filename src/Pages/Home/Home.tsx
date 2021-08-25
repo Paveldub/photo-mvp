4c47fbe2961cf004fb4596f413d3597a56e8c42e
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-08-25 15:15:06 +0300

    images fixes

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import { Link } from 'react-router-dom'
-import mainImage from '../../assets/images/main-image.jpg'
+import mainImage from '../../assets/images/main-image.webp'
 import { HomeGallery } from '../../Components/Common/HomeGallery'
 import { useTranslate } from '../../Hooks/useTranslate'
 import './styles.scss'
